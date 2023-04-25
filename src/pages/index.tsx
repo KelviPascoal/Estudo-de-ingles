c0e250425598fb8e38338ca3c38412b8ee247c72
kelvi <kelvi_ugo@hotmail.com>
2023-04-25 20:48:16 -0300

    feat: adiciona imagens ao mock

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { bedrooms, kitchen, livingRoom } from "@/mock/data";
+import { bedrooms, kitchen, livingRoom, office } from "@/mock/data";
 import { Button } from "@chakra-ui/react";
 import { Inter } from "next/font/google";
 import Image from "next/image";
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <>
       <Button colorScheme="blue">aaaaaaaaaaaaaaa</Button>
-      {kitchen.objects.map((item, index) => (
+      {office.objects.map((item, index) => (
         <div key={index}>
           <img src={item.src} alt="aaa" width={40} height={40} />
           <p>{item.name}</p>
