d9c7a787a29d0447ce5339ecc0d29a0cae333311
kelvi <kelvi_ugo@hotmail.com>
2023-04-25 19:11:36 -0300

    feat: inicia projeto

---
+++ src/pages/index.tsx
@@ -0,0 +1,20 @@
+import { livingRoom } from "@/mock/data";
+import { Button } from "@chakra-ui/react";
+import { Inter } from "next/font/google";
+import Image from "next/image";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <>
+      <Button colorScheme="blue">aaaaaaaaaaaaaaa</Button>
+      {livingRoom.objects.map((item, index) => (
+        <div key={index}>
+          <img src={item.src} alt="aaa" width={40} height={40} />
+          <p>{item.name}</p>
+        </div>
+      ))}
+    </>
+  );
+}
